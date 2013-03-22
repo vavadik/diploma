fd95cb1f096595f56568873a1d195185ec1db2fe
Vadim <stogrammist@gmail.com>
2013-03-22 22:41:40 +0200

    + first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diploma::Application.config.secret_token = 'cf407f17cb3bf3af01d5b58bc198aff69ac46c84966881bb8cf169307615dc190d8ca2b0712134518148deb119615ffd490316795ac44ecf39e9faa386f4cb33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Diploma::Application.config.session_store :cookie_store, key: '_diploma_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Diploma::Application.config.session_store :active_record_store
